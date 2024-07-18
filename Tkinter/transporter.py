86d21ed60d3d3c2348096eac57c8ff8c4e7e0a12
Meijix <nmejia30@ciencias.unam.mx>
2024-07-17 20:56:54 -0600

    Delete Tkinter files

--- Tkinter/transporter.py
+++
@@ -1,8 +0,0 @@
-from morse.builder.creator import ActuatorCreator
-
-class Transporter(ActuatorCreator):
-    def __init__(self, name=None):
-        ActuatorCreator.__init__(self, name, \
-                                 "baldr.actuators.transporter.Transporter",\
-                                 "transporter")
-
